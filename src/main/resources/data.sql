c4444781029c31b9a574922a185904ed8fc81607
“TomomasaN” <tomo1th.aug@gmail.com>
2023-09-30 18:53:22 +0900

    解体新書、HTML周り完了しました

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,7 @@ INSERT INTO employee
 )
 VALUES
 ('1','Tom',30);
+//初期データを投入するSQ
 
 /* ユーザーマスタ */
 INSERT INTO m_user (
@@ -21,6 +22,7 @@ user_id
 ('system@co.jp', '$2a$10$P74tOcpXNgqpn9jN7Kb2N.9d7crygz0hds9YtHzPjSZXbhIS.jOgm', 'システム管理者', '2000-01-01', 21, 1, 1, 'ROLE_ADMIN')
 ,('user@co.jp', '$2a$10$P74tOcpXNgqpn9jN7Kb2N.9d7crygz0hds9YtHzPjSZXbhIS.jOgm', 'ユーザー1', '2000-01-01', 21, 2, 2, 'ROLE_GENERAL')
 ;
+//ユーザーデータ設定
 
 /* 部署マスタ */
 INSERT INTO m_department (
